b190f8e3cf13607137aefd2897c49cf52cb4844f
daijikuriakose2006 <daijikuriakose2028@cs.sjcetpalai.ac.in>
2025-04-14 12:50:30 +0530

    first commit

--- src/pages/About.tsx
+++ src/pages/About.tsx
@@ -29,7 +29,7 @@ const About = () => {
 
       <Grid container spacing={4}>
         {skills.map((skill, index) => (
-          <Grid item xs={12} sm={6} md={3} key={skill.name}>
+          <Grid> item xs={12} sm={6} md={3} key={skill.name}
             <motion.div
               initial={{ opacity: 0, y: 20 }}
               animate={{ opacity: 1, y: 0 }}
